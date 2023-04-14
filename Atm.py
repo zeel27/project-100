66b6eeb384397213dbca14825ffe541fbe74b981
zeel27 <110594630+zeel27@users.noreply.github.com>
2023-04-14 19:34:24 +1000

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,10 @@
+class Atm(object):
+    def __init__(self,cardNumber,pinNumber):
+        self.cardNumber=cardNumber
+        self.pinNumber=pinNumber
+
+def cashWithdrawl(self):
+    print("your cash has been withdrawn")
+
+def balanceEnqiry(self):
+    print("this is the balance in your account")
